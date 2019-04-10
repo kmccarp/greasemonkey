dda4240d0e73a0b38a86910875c1d2e9c583091d
kwcarpenter <kwcarpenter@gmail.com>
2019-04-10 12:32:46 -0700

    Fix Amazon Invoice GreaseMonkey script
    Amazon recently changed their orders page. As a result, I had to restructure how I'm getting the invoice costs.

--- amazonInvoice.js
+++ amazonInvoice.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name         Amazon.com invoice amounts on order history
 // @namespace    http://tampermonkey.net/
-// @version      1.0.4
+// @version      1.0.5
 // @description  Display invoice amounts on the Amazon order history page. Useful for matching Amazon orders to transactions
-// @author       Kevin McCarpenter (kcarp)
-// @match        https://www.amazon.*/gp/*/order-history*
+// @author       Kevin McCarpenter
+// @match        https://www.amazon.com/gp/*/order-history*
 // @updateURL    https://raw.githubusercontent.com/kwcarpenter/greasemonkey/master/amazonInvoice.js
 // @downloadURL  https://raw.githubusercontent.com/kwcarpenter/greasemonkey/master/amazonInvoice.js
 // @require http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js
@@ -15,38 +15,51 @@
     'use strict';
 
 
-    var invoiceLinks = $('a.a-link-normal').filter(function(index, element) {
-        return $(element).text() == 'Invoice';
+    var orderDetailsLinks = $('a.a-button-text').filter(function(index, element) {
+        return $(element).attr('id').match(/Order-details_\d/);
     });
 
-    invoiceLinks.each(function(index, link) {
-        var costDiv = $('<span></span>');
-        var costTitle = $('<span>Invoice costs: </span>');
-        var costSpan = $('<span>Calculating</span>');
-        $(costDiv).append(costTitle);
-        $(costDiv).append(costSpan);
-        $(link).parent().append(costDiv);
-        $.get($(link).attr('href'))
-        .done(function(response) {
-            // find invoice values
-            var responseDom = $.parseHTML(response);
-            var shipments = $(responseDom).find('tr tr tr tr tr').filter(function(index, element) {
-                return $(element).first('td').text().indexOf('Shipment') > 0;
-            });
 
-            // add invoice values to order dom
-            var costSpanStr = '';
-            shipments.each(function(index, shipment) {
-                var matches = /\$\d+\.\d{2}/.exec($(shipment).text())
-                console.log(matches);
-                for(var i = 0; i < matches.length; i++) {
-                    costSpanStr += (' ' + matches[i]);
-                }
+    orderDetailsLinks.each(function(index, link) {
+        $.get($(link).attr('href')).done(function(orderDetailsResponse) {
+            var initialResponseDOM = $.parseHTML(orderDetailsResponse);
+            var invoiceLinks = $(initialResponseDOM).find('a.a-button-text').filter(function(index, element) {
+                return $(element).text().match(/View or Print invoice/);
             });
-            costSpan.text(costSpanStr);
-        })
-        .fail(function (error) {
-            console.log(error);
+            invoiceLinks.each(function(invoiceIndex, invoiceLink) {
+                var costDiv = $('<div></div>');
+                var costTitle = $('<span>Invoice costs: </span>');
+                var costSpan = $('<span>Calculating</span>');
+                $(costDiv).append(costTitle);
+                $(costDiv).append(costSpan);
+
+                // sorry.
+                $(link).parent().parent().parent().parent().parent().parent().parent().parent().find('.a-row.shipment-top-row').append(costDiv);
+                $.get($(invoiceLink).attr('href'))
+                    .done(function(response) {
+                    // find invoice values
+                    var responseDom = $.parseHTML(response);
+                    console.log(responseDom);
+                    var shipments = $(responseDom).find('tr tr tr tr tr').filter(function(index, element) {
+                        console.log(element);
+                        return $(element).text().match(/Shipment/) && $(element).text().match(/\$\d+\.\d{2}/);
+                    });
+
+                    // add invoice values to order dom
+                    var costSpanStr = '';
+                    shipments.each(function(index, shipment) {
+                        var matches = /\$\d+\.\d{2}/.exec($(shipment).text())
+                        console.log(matches);
+                        for(var i = 0; i < matches.length; i++) {
+                            costSpanStr += (' ' + matches[i]);
+                        }
+                    });
+                    costSpan.text(costSpanStr);
+                })
+                    .fail(function (error) {
+                    console.log(error);
+                });
+            });
         });
     });
 
